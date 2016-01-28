44cf4fc5c67ff718cc7e0215a7a976057a4f413c
Aaron Piotrowski <aaron@trowski.com>
2016-01-28 10:37:16 -0600

    Use separate argument for file ID

--- src/Concurrent/Internal/FileTask.php
+++ src/Concurrent/Internal/FileTask.php
@@ -14,17 +14,23 @@ class FileTask implements Task
     private $operation;
 
     /**
-     * @var int
+     * @var mixed[]
      */
     private $args;
 
     /**
+     * @var string|null
+     */
+    private $id;
+
+    /**
      * @param string $operation
      * @param array $args
+     * @param int $id File ID.
      *
      * @throws \Icicle\Exception\InvalidArgumentError
      */
-    public function __construct($operation, array $args = [])
+    public function __construct($operation, array $args = [], $id = 0)
     {
         if (!strlen($operation)) {
             throw new InvalidArgumentError('Operation must be a non-empty string.');
@@ -32,6 +38,10 @@ class FileTask implements Task
 
         $this->operation = $operation;
         $this->args = $args;
+
+        if (0 !== $id) {
+            $this->id = $this->makeId($id);
+        }
     }
 
     /**
@@ -50,17 +60,15 @@ class FileTask implements Task
                 return [$id, $file->stat()['size'], $file->inAppendMode()];
             }
 
-            if (!isset($this->args[0])) {
+            if (null === $this->id) {
                 throw new FileException('No file ID provided.');
             }
 
-            $id = $this->makeId(array_shift($this->args));
-
-            if (!$environment->exists($id)) {
+            if (!$environment->exists($this->id)) {
                 throw new FileException('No file handle with the given ID has been opened on the worker.');
             }
 
-            if (!($file = $environment->get($id)) instanceof File) {
+            if (!($file = $environment->get($this->id)) instanceof File) {
                 throw new FileException('File storage found in inconsistent state.');
             }
 
@@ -73,7 +81,7 @@ class FileTask implements Task
                     return call_user_func_array([$file, substr($this->operation, 1)], $this->args);
 
                 case 'fclose':
-                    $environment->delete($id);
+                    $environment->delete($this->id);
                     return true;
 
                 default:
